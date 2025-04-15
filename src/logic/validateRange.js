cf7e3403d5e4956449e14daa2d60c0f2c785c252
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2025-04-15 15:04:37 +0100

    Update validaterange method

--- src/logic/validateRange.js
+++ src/logic/validateRange.js
@@ -1,6 +1,6 @@
 function validateRange(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] >= 10 || arr[i][1] >= 10) {
+    if ((arr[i][0] >= 10 || arr[i][1] >= 10) || (arr[i][0] < 0 || arr[i][1] < 0)) {
       return false;
     }
   }
